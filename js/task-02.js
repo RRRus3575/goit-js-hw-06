a508c259cbd47d79bb0680cc88ff4c48ca89650d
Ruslan <ruslan.grudistov@gmail.com>
2023-02-24 16:45:46 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+const createItem = (elements) => {
+  return elements.map((element) => {
+    const item = document.createElement("li");
+    item.classList.add("item");
+    item.textContent = element;
+    return item;
+  });
+};
+
+const itemList = createItem(ingredients);
+listEl.append(...itemList);
