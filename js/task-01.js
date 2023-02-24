57b24b323540ee4291a9961cbd268424673883cb
Ruslan <ruslan.grudistov@gmail.com>
2023-02-24 15:50:58 +0200

    task1

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+"string";
+
+const elements = document.querySelectorAll(".item");
+
+console.log(`Number of categories: `, elements.length);
+
+elements.forEach((el) => {
+  const title = el.firstElementChild.textContent;
+  const listEl = el.lastElementChild.children.length;
+
+  console.log(`Category: `, title);
+  console.log(`Category: `, listEl);
+});
