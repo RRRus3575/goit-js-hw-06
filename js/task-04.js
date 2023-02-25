4db444fb17b552dd894f48f4bbc5bd5f6fc79c50
Ruslan <ruslan.grudistov@gmail.com>
2023-02-25 16:02:37 +0200

    task - 5

--- js/task-04.js
+++ js/task-04.js
@@ -1,13 +1,24 @@
-const buttonDecrement = document.querySelector(".decrement");
-const buttonIncrement = document.querySelector(".increment");
+const buttonDecrement = document.querySelector(
+  `button[data-action="decrement"]`
+);
+const buttonIncrement = document.querySelector(
+  `button[data-action="increment"]`
+);
+const counterValue = document.querySelector("#value");
 
-const counterValue = 0;
+const counter = function ({ step = 1, counterValue = 0 }) {
+  this.value = counterValue;
+  this.step = step;
+};
 
-buttonDecrement.addEventListener("click", () => {
-  counterValue -= 1;
-});
-buttonIncrement.addEventListener("click", () => {
-  counterValue += 1;
-});
+counter.handleClickDecrement = () => {
+  console.log("buy");
+  return (value -= 1);
+};
+counter.handleClickIncrement = () => {
+  // return (value += 1);
+  console.log("buy");
+};
 
-console.log(counterValue);
+buttonDecrement.addEventListener("click", handleClickDecrement);
+buttonIncrement.addEventListener("click", handleClickIncrement);
---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+const inputEl = document.querySelector("#name-input");
+const spanEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", handleInputChange);
+
+function handleInputChange(event) {
+  if (event.currentTarget.value.length > 0) {
+    spanEl.textContent = event.currentTarget.value;
+  } else {
+    spanEl.textContent = "Anonymous";
+  }
+}
