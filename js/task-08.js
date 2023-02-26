2b0ca15256ba62fb32ac2bcac90a3b64f21122fb
Ruslan <ruslan.grudistov@gmail.com>
2023-02-26 14:04:31 +0200

    task-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,10 @@
+const formEl = document.querySelector(".login-form");
+const inputsEl = document.querySelectorAll("input");
+console.log(inputsEl);
 
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(formEl.value);
+
+  event.target.reset();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const spanEl = document.querySelector(".color");
+const body = document.querySelector("body");
+const buttonChange = document.querySelector(".change-color");
+console.log(spanEl);
+console.log(body);
+console.log(buttonChange);
+
+buttonChange.addEventListener("click", (event) => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = `${color}`;
+  spanEl.textContent = `${color}`;
+});
